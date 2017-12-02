c41f0b4be9c2ab66540322df147110edf1939968
Xu Yan <yan@d-69-91-151-12.dhcp4.washington.edu>
2017-12-02 10:55:57 -0800

    Initial Commit

---
+++ CE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CE
+//
+//  Created by Xu Yan on 12/2/17.
+//  Copyright © 2017 Xu Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
